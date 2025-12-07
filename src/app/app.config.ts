bae5df275bc2d418dfb9ec75f0c944dba912ed53
Gonzalo Córdova Gonzales <gonzalocoop@gmail.com>
2025-12-07 02:22:02 -0500

    aa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, inject, PLATFORM_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
@@ -6,10 +6,19 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { JwtModule } from '@auth0/angular-jwt';
+import { isPlatformBrowser } from '@angular/common';
 
 export function tokenGetter() {
+  const platformId = inject(PLATFORM_ID);
+
+  // Evita usar sessionStorage en el servidor
+  if (!isPlatformBrowser(platformId)) {
+    return null;
+  }
+
   return sessionStorage.getItem('token');
 }
+
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),
     provideHttpClient(withInterceptorsFromDi()), provideAnimationsAsync(),
